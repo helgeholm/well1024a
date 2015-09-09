88e607886ee08ee25ac6b68c9028ec1d2138e124
Helge Skogly Holm <helge.holm@gmail.com>
2015-09-09 12:52:40 +0200

    Updated browser version.

--- browser/well1024a.js
+++ browser/well1024a.js
@@ -7,8 +7,8 @@ function well1024a (entropy_) {
                   0, 0, 0, 0, 0, 0, 0, 0 ];
     var state_i = 0;
     var z0, z1, z2;
-    function maToPos (t, v) { return v ^ (v >>> t); }
-    function maToNeg (t, v) { return v ^ (v << -t); }
+    function mat0pos (t, v) { return v ^ (v >>> t); }
+    function mat0neg (t, v) { return v ^ (v << -t); }
     
     function init (entropy) {
         for (var i = 0; i < state.length; i++)
@@ -42,12 +42,12 @@ function well1024a (entropy_) {
     
     function getUInt32 () {
         z0 = state[(state_i + 31) & 0x1f];
-        z1 = state[state_i] ^ maToPos(8, state[(state_i + m1) & 0x1f]);
-        z2 = maToNeg(-19, state[(state_i + m2) & 0x1f]) ^ maToNeg(-14, state[(state_i + m3) & 0x1f]);
+        z1 = state[state_i] ^ mat0pos(8, state[(state_i + m1) & 0x1f]);
+        z2 = mat0neg(-19, state[(state_i + m2) & 0x1f]) ^ mat0neg(-14, state[(state_i + m3) & 0x1f]);
         state[state_i] = z1 ^ z2;
-        state[(state_i + 31) & 0x1f] = maToNeg(-11, z0) ^ maToNeg(-7, z1) ^ maToNeg(-13, z2);
+        state[(state_i + 31) & 0x1f] = mat0neg(-11, z0) ^ mat0neg(-7, z1) ^ mat0neg(-13, z2);
         state_i = (state_i + 31) & 0x1f;
-        return state[state_i] + 0x80000000;
+        return state[state_i] >>> 0;
     }
     
     init(entropy);
